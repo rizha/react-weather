34abfeb072111c1acddb6836bea01fce214a5f2d
rizha <rizha.musthafa@gmail.com>
2017-04-09 17:48:05 +0700

    add Examples component

---
+++ app/components/Examples.jsx
@@ -0,0 +1,11 @@
+const React = require('react');
+
+const Examples = React.createClass({
+  render: function() {
+    return (
+      <h3>Examples Component</h3>
+    );
+  }
+})
+
+module.exports = Examples;
