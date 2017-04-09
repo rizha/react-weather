978fe20991974a3f00f745e7835cad993aeb4c92
rizha <rizha.musthafa@gmail.com>
2017-04-09 23:19:17 +0700

    Styling Main and Example page

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,11 +1,19 @@
 const React = require('react');
-
+const { Link } = require('react-router');
 
 const Examples = (props) => {
   return (
     <div>
-      <h3>Examples</h3>
-      <p>Welcome to exmples page!</p>
+      <h1 className="text-center">Examples</h1>
+      <p>Here are a few example locations to try out:</p>
+      <ol>
+        <li>
+          <Link to='/?location=Jakarta'>Jakarta, ID</Link>
+        </li>
+        <li>
+          <Link to='/?location=Madiun'>Madiun, ID</Link>
+        </li>
+      </ol>
     </div>
 
   );
