8611951563707635aa2290a6015156b623522071
rizha <rizha.musthafa@gmail.com>
2017-04-09 17:47:43 +0700

    add about component

---
+++ app/components/About.jsx
@@ -0,0 +1,11 @@
+const React = require('react');
+
+const About = React.createClass({
+  render: function() {
+    return (
+      <h3>About Component</h3>
+    );
+  }
+})
+
+module.exports = About;
