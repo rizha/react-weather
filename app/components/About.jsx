a5cd0bef7e92124e78838bdc87433ee92581f039
rizha <rizha.musthafa@gmail.com>
2017-04-09 23:29:03 +0700

    fix typo and add target blank to link

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -5,14 +5,14 @@ const About = (props) => {
     <div>
       <h1 className="text-center">About</h1>
       <p>This is a weather application build on React.</p>
-      <p>Here are som of the tools I've used</p>
+      <p>Here are some of the tools I've used</p>
       <ul>
         <li>
-          <a href="https://facebook.github.io/react">React</a> - This was the
+          <a href="https://facebook.github.io/react" target="_blank">React</a> - This was the
             Javascript framework used.
         </li>
         <li>
-          <a href="http://openweathermap.org">Open Weather Map</a> - I used
+          <a href="http://openweathermap.org" target="_blank">Open Weather Map</a> - I used
             Open Weather Map to search for weather data by city name.
         </li>
       </ul>
